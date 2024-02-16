c999a4a0cc54fab03a805fb6513a81bc1f7b4aea
Suraj Pandey <111424454+SurajjBhardwaj@users.noreply.github.com>
2024-02-16 21:59:21 +0530

    Updated the connection code to check whether the database is already connected or not

--- backend/db.js
+++ backend/db.js
@@ -2,16 +2,19 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-// import dotenv from "dotenv";
-// dotenv.config();
 console.log(process.env.MONGO_URI);
 
-const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/registration";
+const mongoURI =
+  process.env.MONGO_URI || "mongodb://localhost:27017/registration";
 
 const connect = async () => {
   try {
-    await mongoose.connect(mongoURI, { useNewUrlParser: true });
-    console.log("✅ Database Connected");
+    if (mongoose.connection.readyState === 0) {
+      await mongoose.connect(mongoURI, { useNewUrlParser: true });
+      console.log("✅ Database Connected");
+    } else {
+      console.log("Database already connected");
+    }
   } catch (error) {
     console.error("Error connecting to the database:", error.message);
   }
